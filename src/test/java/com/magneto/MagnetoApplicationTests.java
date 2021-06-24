fbeaf6c081e19e27871716fc770084343f096a8b
Sergio Eduardo Martinez <sergioeduardo.martinez@bbva.com>
2021-06-23 20:34:21 -0500

    inclusion script base de datos

--- src/test/java/com/magneto/MagnetoApplicationTests.java
+++ src/test/java/com/magneto/MagnetoApplicationTests.java
@@ -1,19 +1,12 @@
 package com.magneto;
 
-import com.magneto.service.MutantController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest
 class MagnetoApplicationTests {
-    @Autowired
-    MutantController mutantController;
 
     @Test
     void contextLoads() {
